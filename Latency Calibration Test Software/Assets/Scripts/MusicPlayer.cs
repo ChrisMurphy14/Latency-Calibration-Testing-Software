8d1155c8e9f421ef1ca517688ffd0968e4854cbe
LEAKYFINGERS <weepingdigits@gmail.com>
2019-10-06 12:29:53 +1100

    Started implementing 'Song' class

---
+++ Latency Calibration Test Software/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,51 @@
+﻿//////////////////////////////////////////////////
+// Author:              Chris Murphy
+// Date created:        30.09.19
+// Date last edited:    30.09.19
+//////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+// The class used to play a song and display the associated rhythm gameplay input prompts.
+public class MusicPlayer : MonoBehaviour
+{
+    public Song SongToPlay;
+    public float InputWindowDuration;
+    public float DebugLatencyOffset; // DEBUG 
+    // public float VideoLatencyOffset;
+    // public float AudioLatencyOffset;
+
+        
+    private AudioSource audioSource;       
+    private float songPosInSeconds; // The current play position of the song in seconds.    
+    private float songPosInBeats; // The current play position of the song in beats.   
+    private float songStartTime;  // The time value when the song started playing.    
+    private int prevBeatCount; // DEBUG
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = SongToPlay.AudioTrack;
+        songStartTime = (float)AudioSettings.dspTime;
+    }
+
+    private void Start()
+    {              
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        songPosInSeconds = (float)(AudioSettings.dspTime - songStartTime) - DebugLatencyOffset;
+        songPosInBeats = songPosInSeconds / SongToPlay.SecondsPerBeat;
+
+        if ((int)songPosInBeats > prevBeatCount)
+            Debug.Log("Beat: " + (int)songPosInBeats);
+        //    Camera.main.GetComponent<Camera>().backgroundColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        prevBeatCount = (int)songPosInBeats;
+    }
+}
---
+++ Latency Calibration Test Software/Assets/Scripts/Song.cs
@@ -0,0 +1,46 @@
+﻿//////////////////////////////////////////////////
+// Author:              Chris Murphy
+// Date created:        05.10.19
+// Date last edited:    05.10.19
+//////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+// A note for the player to input as part of a song.  
+[System.Serializable]
+public struct SongInputNote
+{
+    // The pitch values for each note input.
+    public enum NotePitch
+    {
+        Low,
+        Mid,
+        High
+    }
+        
+    public NotePitch Pitch; // The pitch of the note, used to determine the appropriate input key and visual prompt.    
+    public float BeatPosInSong; // The position of the note in the song in beats.
+}
+
+
+// A song class containing both the audio track and additional rhythm gameplay data.
+public class Song : MonoBehaviour
+{   
+    public AudioClip AudioTrack;   
+    public float BPM;    
+    public List<SongInputNote> InputNotes; // The list containing all of the notes to be input throughout the playtime of the song.
+    
+    public float SecondsPerBeat
+    {
+        get { return 60.0f / BPM; }
+    }
+    
+
+    private void Awake()
+    {        
+        InputNotes = InputNotes.OrderBy(x => x.BeatPosInSong).ToList(); // Re-orders the notes according to play position.
+    }
+}
